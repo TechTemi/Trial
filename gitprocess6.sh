ee71bce3d46c72974cb3b03a19ce1799f6bae57a
TechTemi <122737846+TechTemi@users.noreply.github.com>
2023-03-24 16:07:14 -0400

    final trial

--- gitprocess6.sh
+++ gitprocess6.sh
@@ -5,6 +5,7 @@
 May God deliver us from the Agbado Demon.
 A sick man who is about to make a sick country worse.
 I am so tired of this jenkins-git integration not working oo.
+Final trial before heading out to work.
 emi
 
 
