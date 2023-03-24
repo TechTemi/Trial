d237f1cc713a77794bf1afd35f8ab8773d0efe48
TechTemi <122737846+TechTemi@users.noreply.github.com>
2023-03-24 14:56:12 -0400

    I added an 's' to reign on line 3.

--- gitprocess6.sh
+++ gitprocess6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 <<emi
-  After all is said and done. The Agbado Republic reign Supreme.
+  After all is said and done. The Agbado Republic reigns Supreme.
 
 May God deliver us from the Agbado Demon.
 
